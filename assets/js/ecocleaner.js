a353f0898ddb901eff7f93fd645e4928fc2f005d
Karolina Pikus <karolina@pikus.it>
2019-11-18 02:27:09 +0100

    Stylesheets and app script updated

--- assets/js/ecocleaner.js
+++ assets/js/ecocleaner.js
@@ -2,7 +2,7 @@ jQuery(document).ready(function () {
 	let razem = jQuery(".razem-cena");
 
     razem.on("click", function (event) {
-	event.preventDefault()
+	event.preventDefault();
 	let naczynia = jQuery("input[name='naczynia-cena']").val();
     let podlogi = jQuery("input[name='podlogi-cena']").val();
     let dywany = jQuery("input[name='dywany-cena']").val();
@@ -12,37 +12,37 @@ jQuery(document).ready(function () {
     let inne = jQuery("input[name='inne-cena']").val();
 		
 
-		let naczyniaVal = 0
-		let podlogiVal = 0
-		let dywanyVal = 0
-		let kuchniaVal = 0
-		let lazienkaVal = 0
-		let oknaVal = 0
-		let inneVal = 0
+		let naczyniaVal = 0;
+		let podlogiVal = 0;
+		let dywanyVal = 0;
+		let kuchniaVal = 0;
+		let lazienkaVal = 0;
+		let oknaVal = 0;
+		let inneVal = 0;
 		
 		if (naczynia.length != 0) {
 			naczyniaVal = parseFloat(naczynia);
-		}
+		};
 		if (podlogi.length != 0){
 			podlogiVal = parseFloat(podlogi);
-		}
+		};
 		if(dywany.length != 0){
 			dywanyVal = parseFloat(dywany);
-		}
+		};
 		if (kuchnia.length != 0){
 			kuchniaVal = parseFloat(kuchnia);
-		}
+		};
 		if (lazienka.length != 0){
 			lazienkaVal = parseFloat(lazienka);
-		}
+		};
 		if (okna.length != 0){
-			oknaVal = parseFloat(okna)
-		}
+			oknaVal = parseFloat(okna);
+		};
 		if (inne.length != 0){
 			inneVal = parseFloat(inne);
-		}
+		};
 
         let sum = naczyniaVal + podlogiVal + dywanyVal + kuchniaVal + lazienkaVal + oknaVal + inneVal;
-        razem.text(`${sum} zł`)
-    })
-})
+        razem.text(`${sum} zł`);
+    });
+});
